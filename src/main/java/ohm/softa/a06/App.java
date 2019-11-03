11ea18080de6b9346db5ff7ca10ddac0ef016075
gsellja67901 <jacob.gsell@gmx.de>
2019-11-03 12:31:36 +0100

    :(

--- src/main/java/ohm/softa/a06/App.java
+++ src/main/java/ohm/softa/a06/App.java
@@ -1,13 +1,28 @@
 package ohm.softa.a06;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import ohm.softa.a06.model.Joke;
+import ohm.softa.a06.adapters.JokeAdapter;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.util.List;
 
 /**
  * @author Peter Kurfer
  * Created on 11/10/17.
  */
-public class App {
+public class App implements ICNDBApi {
+
+	static final String BASE_URL = "http://api.icndb.com/";
+
+	Gson gson = new GsonBuilder().setLenient().create();
+
+	Retrofit.Builder builder = new Retrofit.Builder()
+			.baseUrl(BASE_URL)
+			.addConverterFactory(GsonConverterFactory.create());
 
 	public static void main(String[] args) {
 		// TODO fetch a random joke and print it to STDOUT
@@ -20,4 +35,18 @@ public class App {
 		System.out.println(json);
 	}
 
+	@Override
+	public Call<Joke> getRandomJoke() {
+		return null;
+	}
+
+	@Override
+	public Call<List<Joke>> getRandomJokes() {
+		return null;
+	}
+
+	@Override
+	public Call<Joke> getJokeById(int number) {
+		return null;
+	}
 }
---
+++ src/main/java/ohm/softa/a06/adapters/JokeAdapter.java
@@ -0,0 +1,53 @@
+package ohm.softa.a06.adapters;
+
+import com.google.gson.Gson;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import ohm.softa.a06.model.Joke;
+
+import java.io.IOException;
+
+public class JokeAdapter extends TypeAdapter<Joke> {
+
+	private final Gson gson;
+
+	public JokeAdapter() {
+		gson = new Gson();
+	}
+
+	@Override
+	public void write(JsonWriter out, Joke value) throws IOException {
+
+	}
+
+	@Override
+	public Joke read(JsonReader in) throws IOException {
+		Joke result = null;
+
+		in.beginObject();
+
+		while(in.hasNext())
+		{
+			switch (in.nextName())
+			{
+				case "type":
+					if(!in.nextString().equals("success"))
+					{
+						throw new IOException();
+					}
+					break;
+
+				case "value":
+					result = gson.fromJson(in, Joke.class);
+
+				default:
+					throw new IllegalStateException("Unexpected value: " + in.nextName());
+			}
+		}
+
+		in.endObject();
+
+		return result;
+	}
+}
